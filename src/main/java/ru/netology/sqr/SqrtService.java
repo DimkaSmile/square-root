adb4c28b989b7394d2a9234f05fdd12df8e315b8
DimkaSmile <95303265+DimkaSmile@users.noreply.github.com>
2022-03-21 16:48:27 +0300

    Update SqrtService.java

--- src/main/java/ru/netology/sqr/SqrtService.java
+++ src/main/java/ru/netology/sqr/SqrtService.java
@@ -5,7 +5,7 @@ public class SqrtService {
     public int rangeOfNumbers(int lowerLimit, int upperLimit) {
 
         int counter =0;
-        for (int i = 0; i <= 9; i++) {
+        for (int i = 10; i <= 99; i++) {
             if ((i * i >= lowerLimit) && (i * i <= upperLimit)) {
                     counter++;
                 }
